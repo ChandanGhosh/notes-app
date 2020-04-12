b985d9d655db13f5dc71f5e7b9dbed0f342dcd43
chandan.ghosh <chandan.kumar.ghosh@ericsson.com>
2020-04-12 20:13:53 +0530

    list functionality added

--- notes/notesutil.go
+++ notes/notesutil.go
@@ -29,6 +29,16 @@ func (n *NoteUtils) NewCliApp() *cli.App {
 // getCommands ...
 func (n *NoteUtils) getCommands() []*cli.Command {
 	return []*cli.Command{
+
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "list notes",
+			Action: func(c *cli.Context) error {
+				return n.listNotes()
+
+			},
+		},
 		{
 			Name:    "add",
 			Aliases: []string{"a"},
@@ -46,8 +56,8 @@ func (n *NoteUtils) getCommands() []*cli.Command {
 				},
 			},
 			Action: func(c *cli.Context) error {
-				n.addNote(c.String("title"), c.String("body"))
-				return nil
+				return n.addNote(c.String("title"), c.String("body"))
+
 			},
 		},
 		{
@@ -62,12 +72,7 @@ func (n *NoteUtils) getCommands() []*cli.Command {
 				},
 			},
 			Action: func(c *cli.Context) error {
-				note, err := n.readNote(c.String("title"))
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(note.Body)
-				return nil
+				return n.readNote(c.String("title"))
 			},
 		},
 		{
@@ -82,48 +87,81 @@ func (n *NoteUtils) getCommands() []*cli.Command {
 				},
 			},
 			Action: func(c *cli.Context) error {
-				n.removeNote(c.String("title"))
-				return nil
+				return n.removeNote(c.String("title"))
+			},
+		},
+		{
+			Name:    "update",
+			Aliases: []string{"u"},
+			Usage:   "update a note",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:    "title",
+					Usage:   "note title",
+					Aliases: []string{"t"},
+				},
+				&cli.StringFlag{
+					Name:    "body",
+					Usage:   "note body",
+					Aliases: []string{"b"},
+				},
+			},
+			Action: func(c *cli.Context) error {
+				return n.updateNote(c.String("title"), c.String("body"))
 			},
 		},
 	}
 }
 
+// listNotes ...
+func (n *NoteUtils) listNotes() error {
+	n.Notes = loadNotes()
+	if len(n.Notes) == 0 {
+		return errors.New("no notes found")
+	}
+	for _, note := range n.Notes {
+		fmt.Printf("%s:\t%s\n", note.Title, note.Body)
+	}
+	return nil
+}
+
 // addNote ...
-func (n *NoteUtils) addNote(title, body string) {
+func (n *NoteUtils) addNote(title, body string) error {
 	notes := n.Notes
 	for _, note := range notes {
 		if note.Title == title {
-			fmt.Println("note title already taken")
-			return
+			return errors.New("note title already taken")
 		}
 	}
 	notes = append(notes, Note{Title: title, Body: body})
 	n.saveNotes(notes)
 	fmt.Println("New note added!")
+	return nil
 }
 
 // ReadNote ...
-func (n *NoteUtils) readNote(title string) (Note, error) {
+func (n *NoteUtils) readNote(title string) error {
 	notes := n.Notes
 	for _, note := range notes {
 		if note.Title == title {
-			return note, nil
+			fmt.Println(note.Body)
+			return nil
 		}
 	}
-	return Note{}, errors.New("No note found")
+	return errors.New("No note found")
 }
 
 // RemoveNote ...
-func (n *NoteUtils) removeNote(title string) {
+func (n *NoteUtils) removeNote(title string) error {
 	notes := n.Notes
 	noteIndex := getNoteIndex(title)
 	if noteIndex == -1 {
-		fmt.Println("No notes found")
+		return errors.New("No notes found")
 	}
 	notes = append(notes[:noteIndex], notes[noteIndex+1:]...)
 	n.saveNotes(notes)
 	fmt.Println("Note removed!")
+	return nil
 }
 
 func getNoteIndex(title string) int {
@@ -133,6 +171,18 @@ func getNoteIndex(title string) int {
 		}
 	}
 	return -1
+}
+
+func (n *NoteUtils) updateNote(title, body string) error {
+	noteIndex := getNoteIndex(title)
+	if noteIndex == -1 {
+		return errors.New("No note found to update")
+	}
+
+	n.Notes[noteIndex].Body = body
+	n.saveNotes(n.Notes)
+	fmt.Println("Note updated!")
+	return nil
 }
 
 func (n *NoteUtils) saveNotes(notes []Note) {
